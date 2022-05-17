f7cb8802379ebcbb0f59e35a39538058d0631f5d
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-05-16 21:05:23 -0600

    Adding fakerjs to provide avatar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import faker from "@faker-js/faker";
 
 const App = () => {
-  return <div>App</div>;
+  console.log(faker.image.avatar());
+  const image = faker.image.avatar();
+  return (
+    <div>
+      <div className="ui container comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img src={image} alt="avatar" />
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Sam
+            </a>
+            <div className="metadata">
+              <span className="date">Today at 6:00PM</span>
+            </div>
+            <div className="text">Nice Blog Post!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
